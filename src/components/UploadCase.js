6f39a260346e8c47ade5dfc48b82b137389c232c
pcion <patrick.ciongoli@gmail.com>
2023-04-13 15:14:49 -0700

    upload case updates

--- src/components/UploadCase.js
+++ src/components/UploadCase.js
@@ -6,31 +6,85 @@ import "../styles/UploadCase.css";
 const UploadCase = () => {
    const listData = [
       {
-         title: "List Item 1",
-         content: "Content for List Item 1",
+         title: "CEREC",
+         content: (
+            <>
+               <p>Create or Login to your CEREC Connect account</p>
+               <p>Select “MY CEREC CONNECT”</p>
+               <p>Type “Alchemy Dental” in the box for Company Name</p>
+               <p>Click FIND</p>
+               <p>Check the box at the bottom left for “Alchemy Dental”</p>
+               <p>Click ADD to complete the registration</p>
+            </>
+         ),
       },
       {
-         title: "List Item 2",
-         content: "Content for List Item 2",
+         title: "iTero Element R",
+         content: (
+            <>
+               <p>Go to us.3shapecommunicate.com in a web browser</p>
+               <p>
+                  Connect with Alchemy Dental by searching
+                  alchemydental@gmail.com
+               </p>
+               <p>
+                  After connecting with us as a Lab, select Alchemy Dental when
+                  sending files
+               </p>
+            </>
+         ),
       },
       {
-         title: "List Item 3",
-         content: "Content for List Item 3",
+         title: "3M True Definition Scanner",
+         content: (
+            <>
+               <p>Contact 3M™ support at 800-634-2249 select Option 3</p>
+               <p>Select Option 1 and request support Alchemy Dental to be</p>
+               <p>added to your scanner</p>
+               <p>
+                  3M™ will then confirm with Alchemy Dental and add connection
+                  remotely
+               </p>
+            </>
+         ),
       },
       {
-         title: "List Item 4",
-         content: "Content for List Item 4",
+         title: "Carestream",
+         content: (
+            <>
+               <p>Visit Carestream Connect on your scanner</p>
+               <p>Search for Alchemy Dental</p>
+               <p>Add Alchemy Dental</p>
+               <p>Select Alchemy Dental when submitting scans raw STL files</p>
+               <p>
+                  Email Alchemy Dental at{" "}
+                  <a href="mailto:alchemydental@gmail.com">
+                     alchemydental@gmail.com
+                  </a>
+               </p>
+            </>
+         ),
       },
    ];
 
    return (
       <Container>
          <h2>How to Upload a Case</h2>
+         <h4>CHOOSE YOUR BRAND AND FOLLOW THE STEPS BELOW</h4>
+         <h6>
+            Sending a case to Alchemy Dental Lab is simple and efficient thanks
+            to digital scanning technology. Find the name of your scanner below
+            and expand the section for the instruction on how to add Alchemy
+            Dental as the default destination for your preferred device. We
+            support all major digital scanning platforms, including
+            SironaCEREC®, the Align Technology iTero™, Carestream, E4D-Planmeca,
+            3M Tm True Definition Scanner, and 3Shapes TRIOS®
+         </h6>
          <ListGroup>
             {listData.map((item, index) => (
                <ListGroup.Item key={index}>
                   <h5>{item.title}</h5>
-                  <p>{item.content}</p>
+                  <div>{item.content}</div>
                </ListGroup.Item>
             ))}
          </ListGroup>
